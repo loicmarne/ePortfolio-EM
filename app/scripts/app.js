06233d6e60736286072bf0df1c5803748fabb291
loic marne <loic marne>
2017-01-04 20:29:07 +0100

    initialisation Angular via Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ePortfolioEmApp
+ * @description
+ * # ePortfolioEmApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ePortfolioEmApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ePortfolioEmApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ePortfolioEmApp
+ */
+angular.module('ePortfolioEmApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
